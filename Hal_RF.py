a7135dd92d4797312803db71473984972fa13b81
wan19990901 <wanguangya@gmail.com>
2019-11-01 02:28:47 -0500

    update

--- Hal_RF.py
+++ Hal_RF.py
@@ -1,36 +1,27 @@
 import numpy as np
 import time
-import scipy.stats as stats
 import pandas as pd
-from matplotlib import pyplot as plt
 from sklearn.ensemble import RandomForestClassifier
-from sklearn import metrics
 from sklearn.model_selection import GridSearchCV, train_test_split
-from sklearn.linear_model import LinearRegression
-from scipy import stats
 from sklearn.metrics import log_loss
 import pickle
-from sklearn.calibration import CalibratedClassifierCV, calibration_curve
-from sklearn.metrics import (brier_score_loss, precision_score, recall_score,
-                             f1_score)
+
 time_start = time.time()
 data = pd.read_sas('/home/shared/cprhd/DATA_CPRHD_SES/wnv_2245new.sas7bdat') # In the Cook_Dupage Directory
 print("Data read in:", time.time() - time_start)
 
 time_start = time.time()
-x_selected = data[data.drop(columns=['wnvbinary','yrweeks','yrwksfid','yr_hexid','year']).columns[[7,8,9,10,16,17,20]]].values
+data = data.drop(columns=['wnvbinary','yrweeks','yrwksfid','yr_hexid','year'])
+
+x_selected = data.drop(columns=data.columns[[4,5,25,26,-17,-16,-15,-14,-13,-12,-11,-10,-9,-8,-6]]).values
 y_selected = data['wnvbinary'].values
 print("Data selected in:", time.time() - time_start)
-
-
-
 time_start = time.time()
-trainX_sel, testX_sel, trainY_sel, testY_sel = train_test_split(x_selected, y_selected, test_size = 0.2, shuffle = True) # CV
+trainX_sel, testX_sel, trainY_sel, testY_sel = train_test_split(x_selected, y_selected, test_size = 0.2, random_state=1) # CV
 print("data split:", time.time() - time_start)
-
-
+class_weights = class_weight.compute_class_weight('balanced' , classes=np.unique(y),y = y)
 time_start = time.time()
-model_RF_best_2 = RandomForestClassifier(n_estimators=1500,
+model_RF_best_2 = RandomForestClassifier(n_estimators=1000,
                                  n_jobs = -1,
                                  max_features=None,
                                  max_depth=None,
@@ -48,11 +39,12 @@ print("model fit:", time.time() - time_start)
 time_start = time.time()
 param_grid = {
     'bootstrap': [True],
-    'max_depth': [80, None],
-    'max_features': ['sqrt', 4],
-    'min_samples_leaf': [3, 5],
-    'min_samples_split': [8, 12],
-    'n_estimators': [500, 1000]
+    'max_depth': [60,100],
+    'max_features': [4,10],
+    'min_samples_leaf': [5,8],
+    'min_samples_split': [5,8],
+    'n_estimators': [1000]
+    'class_weight': [class_weights,None]
 }
 
 
@@ -63,14 +55,14 @@ time_start = time.time()
 CV_model_RF_3.fit(trainX_sel, trainY_sel)
 print("CV model fit:", time.time() - time_start)
 
+pickle.dump(CV_model_RF_3, open('RF_model', 'wb'))
 
 def model_RF_test(model_RF, dataX, dataY):
     print("Model performance")
     predict_data = model_RF.predict_proba(dataX)
-
     # Some stats
     print("Feature Importance : ")
-    print(model_RF.feature_improtances_)
+    print(model_RF.best_estimator_.feature_importances_)
     print("Total number of WNV occurrence in test set : " + str(len(dataY[dataY > 0])))
 
     print("Number of WNV occurrence the model is able to capture in test set:" + str(
@@ -84,6 +76,5 @@ def model_RF_test(model_RF, dataX, dataY):
 
     return None  # Check how many wnv it predicts
 
-model_RF_test(CV_model_RF_3,testX_sel,testY_sel)
 
-pickle.dump(CV_model_RF_3, open('RF_model', 'wb'))
+model_RF_test(CV_model_RF_3,testX_sel,testY_sel)
--- RF_stats.py
+++ RF_stats.py
@@ -3,8 +3,11 @@ import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import log_loss
+from sklearn.metrics import roc_auc_score
 
-
+"""
+This script generates the stats of the model created in RF_model.py
+"""
 
 def model_RF_test(model_RF, dataX, dataY):
     print("Model performance")
@@ -20,21 +23,23 @@ def model_RF_test(model_RF, dataX, dataY):
 
     print("Log loss : " + str(log_loss(dataY, predict_data)))
 
+    print("AUC: " + str(roc_auc_score(dataY, predict_data[:,1])))
+
     print(
         "This is to test the performance of random forest model, ideally, the logloss is low and also it is able to "
         "capture most of the WNV occurrence")
 
     return None  # Check how many wnv it predicts
 
-data = pd.read_sas('/home/jallen17/Downloads/wnv_2245new.sas7bdat') # In the Cook_Dupage Directory
+data = pd.read_sas('/home/shared/cprhd/DATA_CPRHD_SES/wnv_2245new.sas7bdat') # In the Cook_Dupage Directory
 
 
 
-x_selected = data[data.drop(columns=['wnvbinary','yrweeks','yrwksfid','yr_hexid','year']).columns[[7,8,9,10,16,17,20]]].values
+x_selected = data[data.drop(columns=['wnvbinary','yrweeks','yrwksfid','yr_hexid','year']).columns[:]].values
 y_selected = data['wnvbinary'].values
 
 
 trainX_sel, testX_sel, trainY_sel, testY_sel = train_test_split(x_selected, y_selected, test_size = 0.2, shuffle = True) # CV
 
-loaded_model = pickle.load(open('/home/jallen17/Downloads/RF_model', 'rb'))
+loaded_model = pickle.load(open('/home/jallen17/CPRHD_SES/RF_model', 'rb'))
 model_RF_test(loaded_model, testX_sel, testY_sel)
