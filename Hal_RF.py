52385263eababaf5174c8bb4d0dce32689037726
joshfactorial <jallen17@illinois.edu>
2019-11-07 15:25:38 -0600

    minor cleanup of RF code

--- Hal_RF.py
+++ Hal_RF.py
@@ -8,27 +8,26 @@ import pickle
 from sklearn.utils import class_weight
 
 time_start = time.time()
-data = pd.read_sas('/home/shared/cprhd/DATA_CPRHD_SES/wnv_2245new.sas7bdat') # In the Cook_Dupage Directory
+data = pd.read_sas('/home/shared/cprhd/DATA_CPRHD_SES/wnv_2245new.sas7bdat')  # In the Cook_Dupage Directory
 print("Data read in:", time.time() - time_start)
 
 time_start = time.time()
-x = data.drop(columns=['wnvbinary','yrweeks','yrwksfid','yr_hexid','year','income1'])
+x = data.drop(columns=['wnvbinary', 'yrweeks', 'yrwksfid', 'yr_hexid', 'year', 'income1'])
 
-x_selected = x.drop(columns=x.columns[[4,5,25,26,-17,-16,-15,-14,-13,-12,-11,-10,-9,-8,-6]]).values
+x_selected = x.drop(columns=x.columns[[4, 5, 25, 26, -17, -16, -15, -14, -13, -12, -11, -10, -9, -8, -6]]).values
 y_selected = data['wnvbinary'].values
 print("Data selected in:", time.time() - time_start)
 time_start = time.time()
-trainX_sel, testX_sel, trainY_sel, testY_sel = train_test_split(x_selected, y_selected, test_size = 0.2, random_state=1) # CV
+trainX_sel, testX_sel, trainY_sel, testY_sel = train_test_split(x_selected, y_selected, test_size=0.2, random_state=1) # CV
 print("data split:", time.time() - time_start)
-class_weights = class_weight.compute_class_weight('balanced' , classes=np.unique(y_selected),y = y_selected)
+class_weights = class_weight.compute_class_weight('balanced', classes=np.unique(y_selected), y=y_selected)
 time_start = time.time()
 model_RF_best_2 = RandomForestClassifier(n_estimators=1000,
-                                 n_jobs = -1,
-                                 max_features=None,
-                                 max_depth=None,
-                                 bootstrap=True,
-                                 min_samples_leaf=2
-                                 )
+                                         n_jobs=-1,
+                                         max_features=None,
+                                         max_depth=None,
+                                         bootstrap=True,
+                                         min_samples_leaf=2)
 print("Classifier established:", time.time() - time_start)
 
 
@@ -36,21 +35,18 @@ time_start = time.time()
 model_RF_best_2.fit(trainX_sel, trainY_sel)
 print("model fit:", time.time() - time_start)
 
-
-
-
 time_start = time.time()
 
 class_weights = {0: class_weights[0], 1: class_weights[1]}
 
 param_grid = {
     'bootstrap': [True],
-    'max_depth': [60,100],
-    'max_features': [4,10],
-    'min_samples_leaf': [5,8],
-    'min_samples_split': [5,8],
+    'max_depth': [60, 100],
+    'max_features': [4, 10],
+    'min_samples_leaf': [5, 8],
+    'min_samples_split': [5, 8],
     'n_estimators': [1000],
-    'class_weight': [class_weights,None]
+    'class_weight': [class_weights, None]
 }
 
 
@@ -63,9 +59,11 @@ print("CV model fit:", time.time() - time_start)
 
 pickle.dump(CV_model_RF_3, open('RF_model', 'wb'))
 
+
 def model_RF_test(model_RF, dataX, dataY):
     print("Model performance")
     predict_data = model_RF.predict_proba(dataX)
+
     # Some stats
     print("Feature Importance : ")
     print(model_RF.best_estimator_.feature_importances_)
