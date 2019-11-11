5f4bb0c3b06162c623cd1acf2829e623d8757f23
jallen17 <joshfactorial@pop-os.localdomain>
2019-11-11 16:45:11 -0600

    Broke RF model into two stages

---
+++ Hal_RF_model_fit.py
@@ -0,0 +1,44 @@
+import numpy as np
+import time
+import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import GridSearchCV, train_test_split
+from sklearn.metrics import log_loss
+import pickle
+from sklearn.utils import class_weight
+
+"""
+This code fits the Random Forest model. Cross validation is preformed in another
+script.
+"""
+
+time_start = time.time()
+data = pd.read_sas('/home/shared/cprhd/DATA_CPRHD_SES/wnv_2245new.sas7bdat')  # In the Cook_Dupage Directory
+print("Data read in:", time.time() - time_start)
+
+time_start = time.time()
+x = data.drop(columns=['wnvbinary', 'yrweeks', 'yrwksfid', 'yr_hexid', 'year', 'income1'])
+
+x_selected = x.drop(columns=x.columns[[4, 5, 25, 26, -17, -16, -15, -14, -13, -12, -11, -10, -9, -8, -6]]).values
+y_selected = data['wnvbinary'].values
+print("Data selected in:", time.time() - time_start)
+time_start = time.time()
+trainX_sel, testX_sel, trainY_sel, testY_sel = train_test_split(x_selected, y_selected, test_size=0.2, random_state=1) # CV
+print("data split:", time.time() - time_start)
+class_weights = class_weight.compute_class_weight('balanced', classes=np.unique(y_selected), y=y_selected)
+time_start = time.time()
+model_RF_best_2 = RandomForestClassifier(n_estimators=1000,
+                                         n_jobs=-1,
+                                         max_features=None,
+                                         max_depth=None,
+                                         bootstrap=True,
+                                         min_samples_leaf=2)
+print("Classifier established:", time.time() - time_start)
+
+
+time_start = time.time()
+model_RF_best_2.fit(trainX_sel, trainY_sel)
+print("model fit:", time.time() - time_start)
+
+pickle.dump(CV_model_RF_3, open('/home/jallen17/CPRHD_SES/RF_model_fit', 'wb'))
+print("Fitting complete. Model saved as RF_model_fit")
