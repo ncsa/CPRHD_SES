1c0d60fe16beb04e426b917471dff81ab75e079e
Tao Zang <31978589+taozang62@users.noreply.github.com>
2020-10-12 15:55:50 -0500

    dimension bug solved

--- YOLOTrainingPipeline/bbox_util.py
+++ YOLOTrainingPipeline/bbox_util.py
@@ -2,6 +2,7 @@ import imgaug as ia
 from imgaug.augmentables.bbs import BoundingBox, BoundingBoxesOnImage
 import imageio
 import numpy as np
+import matplotlib.pyplot as plt
 
 def convertYOLO2Other(yolo_bb: np.ndarray):
     # This function converts yolo bounding box to coordinates accpeted by imgaug library.
@@ -18,6 +19,7 @@ def convertYOLO2Other(yolo_bb: np.ndarray):
         y2 = center_y + h / 2
         conf = yolo_bb[5]
         imgaug_bb = np.array([class_id, x1, y1, x2, y2, conf])
+        imgaug_bb = imgaug_bb.reshape(1,-1)
     else:
         for n, bb in enumerate(yolo_bb):
             class_id = bb[0]
@@ -53,26 +55,35 @@ def read_ith_pred_result_yolo(data, i, threshold=0):
             pred_bbox_single_img[n, :] = tmp
     return filename, img, pred_bbox_single_img
 
-def draw_ground_truth_label(labelname, img, output_path):
+def draw_ground_truth_label(labelname, img):
     yolo_bb = np.loadtxt('Test/' + labelname)
+    H,W = 640, 640
+    if yolo_bb.size == 0: return yolo_bb
     imgaug_bb = np.zeros(yolo_bb.shape)
-    W = img.shape[0]
-    H = img.shape[1]
-    for n, bb in enumerate(yolo_bb):
-        class_id = bb[0]
-        center_x, center_y, w, h = bb[1] * W, bb[2] * H, bb[3] * W, bb[4] * H
+    if yolo_bb.ndim <= 1:
+        class_id = yolo_bb[0]
+        center_x, center_y, w, h = yolo_bb[1] * W, yolo_bb[2] * H, yolo_bb[3] * W, yolo_bb[4] * H
         x1 = center_x - w / 2
         y1 = center_y - h / 2
         x2 = center_x + w / 2
         y2 = center_y + h / 2
-        imgaug_bb[n] = np.array([class_id, x1, y1, x2, y2])
-
+        imgaug_bb = np.array([class_id, x1, y1, x2, y2])
+        imgaug_bb = imgaug_bb.reshape(1,-1)
+    else:
+        for n, bb in enumerate(yolo_bb):
+            class_id = bb[0]
+            center_x, center_y, w, h = bb[1] * W, bb[2] * H, bb[3] * W, bb[4] * H
+            x1 = center_x - w / 2
+            y1 = center_y - h / 2
+            x2 = center_x + w / 2
+            y2 = center_y + h / 2
+            imgaug_bb[n] = np.array([class_id, x1, y1, x2, y2])
+            
     gt = BoundingBoxesOnImage.from_xyxy_array(imgaug_bb[:, 1:], shape=img.shape)
-
     for n, bb in enumerate(gt):
         bb.label = imgaug_bb[n, 0].astype('int')
-    ia.imshow(gt.draw_on_image(img, size=2))
-    imageio.imwrite(output_path, gt.draw_on_image(img, size=2))
+        
+    return gt.draw_on_image(img, size=2, color=[0,0,255])
 
 def analyze_pred_gt_image(data, i, threshold):
     filename, img, single_image_yolo_pred = read_ith_pred_result_yolo(data, i, threshold)
@@ -84,9 +95,12 @@ def analyze_pred_gt_image(data, i, threshold):
     print(filename)
     print(single_image_yolo_pred)
     print(imgaug_bbox_single_img)
-    ia.imshow(imgaug_bbox_single_img.draw_on_image(img, size=2))
-    imageio.imwrite("sampleImages/pred_bbox_" + filename, imgaug_bbox_single_img.draw_on_image(img, size=2))
+    img_with_yolo = imgaug_bbox_single_img.draw_on_image(img, size=2)
 
     # draw the ground truth
     label_file = filename.split('.jpg')[0] + '.txt'
-    draw_ground_truth_label(label_file, img, 'sampleImages/gt_bbox_' + filename)
+    img_with_gt = draw_ground_truth_label(label_file, img)
+    
+    ia.imshow(np.hstack([img_with_yolo, img_with_gt]))
+    imageio.imwrite("sampleImages/pred_vs_gt_bbox_" + filename, np.hstack([img_with_yolo, img_with_gt]))
+    
